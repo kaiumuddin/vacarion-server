396b277724c127f75d55271c0520c99687b21f79
kaiumuddin <kaium.uddin2909@gmail.com>
2022-11-10 11:21:44 +0600

    first commit

---
+++ __steps.js
@@ -0,0 +1,35 @@
+/* 
+npm init -y
+
+install
+    express
+    cors
+    mongodb
+    dot env
+port 5000
+script "start" : "node index.js"
+
+require('dotenv').config();
+
+Middle Ware
+    app.use(cors());
+    app.use(express.json());
+
+.env 
+    DB_USER=geniusDBUser
+    DB_PASSWORD=xptgoJeZxMUV5uXm
+
+mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rmad3ac.mongodb.net/?retryWrites=true&w=majority`;
+
+.gitignore
+    node_modules
+    .env
+
+*/
+
+/* 
+google search
+    node mongodb crud > uses examples > find operation
+
+*/
