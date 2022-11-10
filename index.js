554dcab7365d46dfe0f6795503d000fe01ac6140
kaiumuddin <kaium.uddin2909@gmail.com>
2022-11-10 18:03:07 +0600

    dot env added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
-
+require('dotenv').config();
 // instance
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,11 +10,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Mongodb
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rmad3ac.mongodb.net/?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://vacarionDBUser:ZWK3X1JbAVSVREpx@cluster0.rmad3ac.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
 
+// Mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rmad3ac.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
 
 async function run() {
     try {
